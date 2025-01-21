9309299697b960c67464a5fd6a95127365db84a3
Samuel Hållman <samuel.hollman@hotmail.com>
2025-01-21 14:25:10 +0200

    next-nueron

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def mysigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1/(1+np.exp(-x)) # This makes the input numbers from nuerons into 0-1 decimal number
 
 mysigmoid(6.7)
 
@@ -14,9 +14,10 @@ class MyNeuron:
         total = np.dot(self.weights,inputs) + self.bias
         return mysigmoid(total)
     
-weights = np.array([0.5,0.8])
-bias = 2
-nueron = MyNeuron(weights,bias)
+weights = np.array([[0.5,0.8], [0.1, 0.3]])
+biases = np.array([2, -1])
+nuerons = MyNeuron(weights,biases)
 
-x = np.array([3,4])
-print(nueron.feedforward(x))
+x = np.array([3,4]) # Nueron weights
+outputs = nuerons.feedforward(x)
+print(outputs)
